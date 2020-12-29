4014f5d2b873037c3c3747383f75f72dfc5afccd
123akg <abhijeetkumargiri7070@gmail.com>
2020-12-29 16:22:56 +0530

    Added: Employee Payroll Backend Application

---
+++ src/main/java/com/capgemini/employeepayrollapp/services/IEmployeePayrollService.java
@@ -0,0 +1,22 @@
+package com.capgemini.employeepayrollapp.services;
+
+import java.util.List;
+
+import com.capgemini.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.capgemini.employeepayrollapp.exceptions.ValidationException;
+import com.capgemini.employeepayrollapp.model.EmployeePayrollData;
+
+public interface IEmployeePayrollService {
+
+	List<EmployeePayrollData> getEmployeeData();
+
+	EmployeePayrollData getEmployeePayrollDataById(int empId) throws ValidationException;
+
+	EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empDto);
+
+	EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empDto) throws ValidationException;
+
+	void deleteEmployeePayrollDataById(int empId);
+	
+	List<EmployeePayrollData> findEmployeesByDepartment(String department);
+}
